634d0108570529e7f24e7a27bf06e3da8b4a83de
Marina Storgatto <mstorgatto@gmail.com>
2018-05-30 19:22:28 -0300

    teste com login pronto

--- testeMercadoLivre/src/main/java/testCase/LoginTestCase.java
+++ testeMercadoLivre/src/main/java/testCase/LoginTestCase.java
@@ -22,7 +22,10 @@ public class LoginTestCase {
 	public void setup() {
 		WebDriverManager.chromedriver().setup();
 		this.driver = new ChromeDriver();
-		this.driver.get("http://demo.virtuemart.net/");
+		this.driver.get("https://www.mercadolivre.com.br/");
+		this.driver.manage().window().maximize();
+		WebElement contaButton = this.driver.findElement(By.xpath("//*[@id=\"nav-header-menu\"]/a[2]"));
+		contaButton.click();
 		
 		this.verificationpoint = new LoginVerificationPoint(driver);
 		
@@ -30,17 +33,35 @@ public class LoginTestCase {
 		
 
 	@Test
-	public void testMain () {
+	public void testMain () throws InterruptedException {
 			
-		WebElement usernameTextField = this.driver.findElement(By.id("modlgn-username"));
-		WebElement passwordTextField = this.driver.findElement(By.id("modlgn-passwd"));
-		WebElement loginButton = this.driver.findElement(By.name("Submit"));
+		WebElement usernameTextField = this.driver.findElement(By.id("user_id"));
+		WebElement usernameButton = this.driver.findElement(By.xpath("//*[@id=\"login_user_form\"]/div[2]/input"));
+		
+		usernameTextField.sendKeys("marina_storgatto@hotmail.com");
+		usernameButton.click();
+
+		Thread.sleep(4000);
+		
+		WebElement passwordTextField = this.driver.findElement(By.id("password"));
+		WebElement passwordButton = this.driver.findElement(By.id("action-complete"));
+				
+		passwordTextField.sendKeys("Amor1963");
+		passwordButton.click();
+		
+		Thread.sleep(4000);
+		
+		WebElement sellButton = this.driver.findElement(By.xpath("/html/body/header/div/div[2]/div/ul/li[6]/a"));
+		sellButton.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement typeButton = this.driver.findElement(By.xpath("//*[@id=\"SERVICES\"]/button"));
+		typeButton.click();
 		
 		
-		usernameTextField.sendKeys("demo"); //inserir palavra demo
-		passwordTextField.sendKeys("demo"); //inserir palavra demo
-		loginButton.click();
-		//loginButton.submit();//mesma ação que click()
+		
+		//loginButton.submit();   //mesma ação que click()
 		
 		verificationpoint.checkHelloMessage();
 	}
